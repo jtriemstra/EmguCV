69d2478f228308c5763ba2c9a21ce34a8c67a0f5
Joel Triemstra <JTriemstra@jtriemstraw7.VML.LAN>
2017-10-26 21:13:35 -0400

    a few tutorials/samples

---
+++ BBasicProgramConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace BBasicProgramConsole
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                Image<Bgr, Byte> My_Image = new Image<Bgr, byte>(Openfile.FileName);
+                //pictureBox1.Image = My_Image.ToBitmap();
+
+                //Show the image using ImageViewer from Emgu.CV.UI
+                ImageViewer.Show(My_Image, "Test Window");
+            }
+        }
+    }
+}
---
+++ CLittleMoreImageProcessing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using System.Windows.Forms;
+
+namespace CLittleMoreImageProcessing
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                Image<Bgr, Byte> My_Image = new Image<Bgr, byte>(Openfile.FileName);
+                //pictureBox1.Image = My_Image.ToBitmap();
+
+                Image<Gray, byte> gray_image = My_Image.Convert<Gray, byte>();
+
+                //Show the image using ImageViewer from Emgu.CV.UI
+                ImageViewer.Show(gray_image, "Test Window");
+            }
+        }
+    }
+}
