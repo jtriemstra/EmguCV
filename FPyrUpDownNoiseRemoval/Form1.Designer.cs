9f0ea01893009f52e12ec2ad907407e6d3430511
Joel Triemstra <JTriemstra@jtriemstraw7.VML.LAN>
2017-11-04 10:32:09 -0400

    use pyr up/down for blurring

---
+++ FPyrUpDownNoiseRemoval/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace FPyrUpDownNoiseRemoval
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numRepetitions = new System.Windows.Forms.NumericUpDown();
+            this.objImageDisplay = new System.Windows.Forms.PictureBox();
+            this.objImageDisplay2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numRepetitions)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objImageDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objImageDisplay2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numRepetitions
+            // 
+            this.numRepetitions.Location = new System.Drawing.Point(12, 12);
+            this.numRepetitions.Name = "numRepetitions";
+            this.numRepetitions.Size = new System.Drawing.Size(58, 20);
+            this.numRepetitions.TabIndex = 0;
+            this.numRepetitions.ValueChanged += new System.EventHandler(this.numReptitions_ValueChanged);
+            // 
+            // objImageDisplay
+            // 
+            this.objImageDisplay.Location = new System.Drawing.Point(12, 47);
+            this.objImageDisplay.Name = "objImageDisplay";
+            this.objImageDisplay.Size = new System.Drawing.Size(421, 266);
+            this.objImageDisplay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.objImageDisplay.TabIndex = 1;
+            this.objImageDisplay.TabStop = false;
+            // 
+            // objImageDisplay2
+            // 
+            this.objImageDisplay2.Location = new System.Drawing.Point(12, 319);
+            this.objImageDisplay2.Name = "objImageDisplay2";
+            this.objImageDisplay2.Size = new System.Drawing.Size(421, 262);
+            this.objImageDisplay2.TabIndex = 2;
+            this.objImageDisplay2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 663);
+            this.Controls.Add(this.objImageDisplay2);
+            this.Controls.Add(this.objImageDisplay);
+            this.Controls.Add(this.numRepetitions);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numRepetitions)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objImageDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.objImageDisplay2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numRepetitions;
+        private System.Windows.Forms.PictureBox objImageDisplay;
+        private System.Windows.Forms.PictureBox objImageDisplay2;
+    }
+}
+
---
+++ FPyrUpDownNoiseRemoval/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+
+namespace FPyrUpDownNoiseRemoval
+{
+    public partial class Form1 : Form
+    {
+        private String m_strSourceImageFileName = @"C:\TestProjects\EmguCV\ShapeTest.jpg";
+        private const String WINDOW_NAME = "Test Window";
+        private Mat m_objSourceImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            m_objSourceImage = new Mat(m_strSourceImageFileName);
+        }
+
+        private void numReptitions_ValueChanged(object sender, EventArgs e)
+        {
+            int intRepetitions = (int) numRepetitions.Value;
+            if (intRepetitions == 0)
+            {
+                DisplayOriginal();
+                return;
+            }          
+
+            DisplayBlurred(intRepetitions);
+            DisplayBlurredExplicitCopy(intRepetitions);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Form load event");
+
+            //ImageViewer.Show(objSourceImage, WINDOW_NAME);
+            DisplayOriginal();
+        }
+
+        private void DisplayOriginal()
+        {
+            objImageDisplay.Image = m_objSourceImage.Bitmap;
+            objImageDisplay2.Image = m_objSourceImage.Bitmap;
+        }
+
+        private void DisplayBlurred(int intRepetitions)
+        {
+            Mat objDestinationImage = m_objSourceImage.Clone();
+
+            for (int i = 0; i < intRepetitions; i++)
+            {
+                CvInvoke.PyrDown(objDestinationImage, objDestinationImage);
+            }
+            for (int i = 0; i < intRepetitions; i++)
+            {
+                CvInvoke.PyrUp(objDestinationImage, objDestinationImage);
+            }
+
+            objImageDisplay.Image = objDestinationImage.Bitmap;
+        }
+
+        private void DisplayBlurredExplicitCopy(int intRepetitions)
+        {
+            if (intRepetitions > 3 || intRepetitions < 1) return;
+
+            Mat objDestinationImage1 = new Mat();
+            Mat objDestinationImage2 = new Mat();
+            Mat objDestinationImage3 = new Mat();
+            Mat objDestinationImage4 = new Mat();
+            Mat objDestinationImage5 = new Mat();
+            Mat objDestinationImage6 = new Mat();
+
+            CvInvoke.PyrDown(m_objSourceImage, objDestinationImage1);
+            if (intRepetitions > 1)
+            {
+                CvInvoke.PyrDown(objDestinationImage1, objDestinationImage2);
+                if (intRepetitions > 2)
+                {
+                    CvInvoke.PyrDown(objDestinationImage2, objDestinationImage3);
+                    CvInvoke.PyrUp(objDestinationImage3, objDestinationImage4);
+                    CvInvoke.PyrUp(objDestinationImage4, objDestinationImage5);
+                    CvInvoke.PyrUp(objDestinationImage5, objDestinationImage6);
+                }
+                else
+                {
+
+                    CvInvoke.PyrUp(objDestinationImage2, objDestinationImage5);
+                    CvInvoke.PyrUp(objDestinationImage5, objDestinationImage6);
+                }
+            }
+            else
+            {
+
+                CvInvoke.PyrUp(objDestinationImage1, objDestinationImage6);
+            }
+
+            objImageDisplay2.Image = objDestinationImage6.Bitmap;
+        }
+
+
+
+    }
+}
+
+
